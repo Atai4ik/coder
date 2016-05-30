9ee7c978fa69a56469a44867a949d89fcd35e2c1
Atai4ik <you@example.com>
2016-05-30 20:06:44 +0600

    First commit

---
+++ script.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+
+
+def fizz_buzz(number):
+    """
+    Функция принимает int.
+    Возвращает Fizz если число делиться на 3,
+    Возвращает Buzz если число делиться на 5.
+    Возвращает FizzBuzz если число делиться на 3 и 5.
+    """
+    if number <= 0:
+        return 'SmallInt'
+
+    if type(number) is str:
+        return 'StringError'
+
+    if number % 3 == 0 and number % 5 == 0:
+        return 'FizzBuzz'
+
+    elif number % 3 == 0:
+        return 'Fizz'
+
+    elif number % 5 == 0:
+        return 'Buzz'
